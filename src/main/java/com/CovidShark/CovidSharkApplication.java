fb51614a076f8fed6cfba03d3a82f3701804ece1
Mustafa Efe Tamyapar <efe.tamyapar@ug.bilkent.edu.tr>
2021-12-18 13:03:56 +0300

    Renamed all
    Added covidShark name to files

---
+++ src/main/java/com/CovidShark/CovidSharkApplication.java
@@ -0,0 +1,13 @@
+package com.CovidShark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidSharkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidSharkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/CovidShark/CovidSharkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CovidShark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidSharkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
