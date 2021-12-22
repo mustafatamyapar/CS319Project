e6a257ebe0240271400f7b84e0015cf3d40fd20c
Burak Ozturk <burakozturk001@gmail.com>
2021-12-22 21:38:41 +0300

    New header done

--- react_frontend/src/components/Home.js
+++ react_frontend/src/components/Home.js
@@ -10,7 +10,6 @@ import DormIcon from "../images/pageIcons/dorm.png";
 import GymIcon from "../images/pageIcons/gym.png";
 import MyProfileIcon from "../images/pageIcons/my_profile_icon.png";
 import RoomHistoryIcon from "../images/pageIcons/room_history_icon.png";
-import {Icon, PageButton, Wrapper, Content} from "./UpperRow/UpperRow.styles";
 import MyCoursesIcon from "../images/pageIcons/my_courses_icon.png";
 import HealthStatusIcon from "../images/pageIcons/health_status_logo.png"
 import FormIcon from "../images/pageIcons/form_icon.png";
