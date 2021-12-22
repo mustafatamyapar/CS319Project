34480a63ad0050bdda534a37a48753783f87eaac
Burak Ozturk <burakozturk001@gmail.com>
2021-12-22 16:16:57 +0300

    React app created for front-end

---
+++ react_frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => (
+    <div className="App">
+        <header className="App-header">
+            <p>
+                Everything started in this blank, dark page.
+            </p>
+        </header>
+    </div>
+);
+
+export default App;
