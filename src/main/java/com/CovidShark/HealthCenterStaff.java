ef83ccd702e6fa4e23007de5b6e9584c94fd9644
melisatuun <melis.atun@ug.bilkent.edu.tr>
2021-12-26 01:11:31 +0300

    comments vol05
    comments on HealthCenterStaff class

--- src/main/java/com/CovidShark/HealthCenterStaff.java
+++ src/main/java/com/CovidShark/HealthCenterStaff.java
@@ -2,7 +2,13 @@ package com.CovidShark;
 
 import java.util.List;
 
+/**
+ * The HealthCenterStaff class extends the BaseUser class.
+ */
 public class HealthCenterStaff extends BaseUser {
+    /**
+     * The appointmentRequests list holds appointment objects.
+     */
     private List<Appointment> appointmentRequests;
 
     public HealthCenterStaff(String idNumber, String name, String email, String phoneNumber, List<Appointment> appointmentRequests) {
@@ -10,38 +16,57 @@ public class HealthCenterStaff extends BaseUser {
         this.appointmentRequests = appointmentRequests;
     }
 
-    /*
-    public BaseUser getUserById(int ID)
-    {
-
+    /**
+     * The getUserById(int ID) operation gets the user by their id number.
+     * @return the user.
+     */
+    public BaseUser getUserById(int ID) {
     }
+
+    /**
+     * The getDiagnosis(int idNumber) operation gets the diagnosis of a user with their id number.
+     * @return the health status of the user.
+     */
     public HealthStatus getDiagnosis(int idNumber) {
-
     }
 
+    /**
+     * The updateDiagnosis(int idNumber) operation updates the diagnosis of a user with their id number.
+     */
     public void updateDiagnosis(int idNumber) {
-
     }
 
+    /**
+     * The seeIndividualsCovidStatus(int idNumber) operation sees the individuals covid status with their id number.
+     * @return the health status of the user.
+     */
     public HealthStatus seeIndividualsCovidStatus(int idNumber) {
-
     }
 
+    /**
+     * The getDormCovidInfo(int idNumber) operation gets the covid information of a dorm.
+     * @return list of students who are covid infected.
+     */
     public List<Student> getDormCovidInfo(int idNumber) {
-
     }
 
+    /**
+     * The updatePCRTest(int idNumber, int result) operation updates the PCR test of a user with their id number.
+     */
     public void updatePCRTest(int idNumber, int result) {
-
     }
 
-    public void giveAppointment(String appointmentCode) { //ID lazim
+    /**
+     * The giveAppointment(String appointmentCode) operation gives appointment to a user if an appointment has been requested.
+     */
+    public void giveAppointment(String appointmentCode) {
         appointmentRequests.add(appointmentCode);
     }
 
-    public void cancelAppointmentRequest(String appointmentCode) { //ID lazim
+    /**
+     * The cancelAppointmentRequest(String appointmentCode) operation cancels an appointment.
+     */
+    public void cancelAppointmentRequest(String appointmentCode) {
         appointmentRequests.remove(appointmentCode);
     }
-
- */
 }
